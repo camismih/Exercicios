44e1e5b8a40fcf0d7cfa65faba62c6f3c71f748d
Camis <camila.mih@outlok.com>
2025-08-28 23:22:28 -0300

    Exercício 2

--- Solution1/PrimeirosExercicios/Program.cs
+++ Solution1/PrimeirosExercicios/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Digite seu nome");
+        /*Console.WriteLine("Digite seu nome");
         var nome = Console.ReadLine();
-        Console.WriteLine($"Seja bem-vindo/A! {nome}");
+        Console.WriteLine($"Seja bem-vindo/A! {nome}"); */
+        var nome = "Vince";
+        var sobreNome = "Vancinni";
+        Console.WriteLine($"Perdeu a memória, foi? Seu Nome é: {nome} E seu sobrenome é {sobreNome}");
     }
 }
