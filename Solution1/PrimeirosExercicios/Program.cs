3f616cd5f97ee84a5fe326a885a4da6589437130
Camis <camila.mih@outlok.com>
2025-08-28 22:34:49 -0300

    Exercício 1]

---
+++ Solution1/PrimeirosExercicios/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PrimeirosExercicios;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome");
+        var nome = Console.ReadLine();
+        Console.WriteLine($"Seja bem-vindo/A! {nome}");
+    }
+}
